b74e2003adb98eca7daaf0047dd83aec6ab4fafb
Keks <keks@htmlacademy.ru>
2022-06-09 20:05:34 +0300

    :heavy_check_mark: Сборка #2

---
+++ 2/js/main.js
@@ -0,0 +1,18 @@
+//Функция, возвращающая случайное целое число из переданного диапазона включительно
+
+const randomNumber = (min, max) => {
+  if (max < min) {
+    return randomNumber(max, min);
+  } else if (min === max) {
+    return Math.floor(max);
+  }
+  return Math.floor(Math.random() * (min - max + 1) + max);
+};
+
+randomNumber(0, 50);
+
+// Функция для проверки максимальной длины строки.
+
+const MAX_LENGTH = 140;
+const IsStringTooLong = (string) => (string.length <= MAX_LENGTH);
+IsStringTooLong('');
