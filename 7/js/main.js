d8651ee1dc7482eb5c8b33b0f5049433eee47e50
Keks <keks@htmlacademy.ru>
2022-07-18 18:18:18 +0300

    :heavy_check_mark: Сборка #7

---
+++ 7/js/main.js
@@ -0,0 +1,7 @@
+import './bigPicture.js';
+import './data.js';
+import './galleryfoto.js';
+import './miniatures.js';
+import './validationForm.js';
+import './uploadImg.js';
+import './overlayImg.js';
